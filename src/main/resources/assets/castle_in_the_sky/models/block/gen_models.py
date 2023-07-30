d026d95237ccc22bedbb94ede56d67ff4eba3322
Song Xia <sx801@nyu.edu>
2023-07-30 09:25:56 -0400

    fix model issues

--- src/main/resources/assets/castle_in_the_sky/models/block/gen_models.py
+++
@@ -1,6 +0,0 @@
-from glob import glob
-for f_src in glob("blue_*.json"):
-    src = open(f_src).read()
-    for color in ["red", "yellow"]:
-        f_out = open(f_src.replace("blue", color), "w")
-        f_out.write(src.replace("blue", color))
